1d5b2bdb42f84af3ae9f03766565a65b77f72c7d
William Chen <chen.will7@gmail.com>
2019-09-09 13:53:21 -0700

    finished most of the UIImage

--- PhotoCollection/PhotoCollection/Views/PhotoCollectionViewCell.swift
+++ PhotoCollection/PhotoCollection/Views/PhotoCollectionViewCell.swift
@@ -12,5 +12,46 @@ class PhotoCollectionViewCell: UICollectionViewCell {
 
     var photo: Photo?
     
+    private func setUpSubviews(){
+        let photoImage = UIImageView()
+        photoImage.translatesAutoresizingMaskIntoConstraints = false
+        photoImage.contentMode = .scaleAspectFit
+        addSubview(photoImage)
+        
+        NSLayoutConstraint(item: photoImage,
+                           attribute: .top,
+                           relatedBy: .equal,
+                           toItem: self,
+                           attribute: .top,
+                           multiplier: 1,
+                           constant: 4).isActive = true
+        
+        NSLayoutConstraint(item: photoImage,
+                           attribute: .leading,
+                           relatedBy: .equal,
+                           toItem: self,
+                           attribute: .leading,
+                           multiplier: 1,
+                           constant: 4).isActive = true
+        
+        NSLayoutConstraint(item: photoImage,
+                           attribute: .trailing,
+                           relatedBy: .equal,
+                           toItem: self,
+                           attribute: .trailing,
+                           multiplier: 1,
+                           constant: -4).isActive = true
+        
+        NSLayoutConstraint(item: photoImage,
+                           attribute: .height,
+                           relatedBy: .equal,
+                           toItem: photoImage,
+                           attribute: .width,
+                           multiplier: 1,
+                           constant: 0).isActive = true
+        
+        self.photoImage = photoImage
+        
+    }
     
 }
